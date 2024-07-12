bf934bd32f0f4fe62b6bfdd6ae9eb53ef1e98890
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-12 10:27:42 +0700

    update font end list

---
+++ list/index.js
@@ -0,0 +1,94 @@
+/*TODO: Hien thi danh sach trang chinh*/
+let temporaryNumber = 0;
+
+function displayList() {
+    $.ajax({
+        method: 'GET', url: 'http://localhost:8080/api/discounts', success: function (data) {
+            let content = `  <tr>
+                 <th>Số thứ tự</th>
+                <th>Thời gian bắt đầu</th>
+                <th>Thời gian kết thúc</th>
+                <th>Mức giảm giá</th>
+                <th>Chi tiết</th>
+                <th> Mức giảm giá</th>
+                <th>Hành động
+                </th>
+                
+        </tr>`
+            for (let i = 0; i < data.length; i++) {
+                content += ` <tr>
+            <td>${i + 1}</td>
+            <td class="">${data[i].title}</td>
+            <td class="">${data[i].startDate}</td>
+            <td class="">${data[i].endDate}</td>
+            <td class = "">${data[i].detail}</td>
+            <td class="">${data[i].discountMoney}</td>
+            <td>
+                <Button type="button" class="btn btn-success"  data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="getTheOldDataOfdiscountFromServer(${data[i].id})"><i class="fa-solid fa-pen-to-square"></i></Button>
+                <span><button type="button" class="btn btn-danger" onclick="deleteById(${data[i].id})"><i class="fa-solid fa-trash"></i></button></span>
+            </td>
+        </tr>`;
+            }
+            content += `</table>`;
+            document.getElementById('discounts').innerHTML = content;
+        }
+    })
+}
+
+displayList();
+
+/*TODO: xoa theo id*/
+function deleteById(id) {
+    $.ajax({
+        type: 'DELETE', url: `http://localhost:8080/api/discounts/${id}`, success: () => {
+            alert("Successfully deleted")
+            displayList();
+        },
+    });
+}
+
+/*TODO: lay du lieu muon chinh sua theo id*/
+function getTheOldDataOfdiscountFromServer(id) {
+    $.ajax({
+        type: 'GET',
+        url: `http://localhost:8080/api/discounts/${id}`,
+        success: function (data) {
+            document.getElementById('price-edit').value = data.price;
+            document.getElementById('color-edit').value = data.color;
+            document.getElementById('description-edit').value = data.description;
+            document.getElementById('quantity-edit').value = data.quantity;
+            document.getElementById('category-edit').value = data.category.name;
+        },
+    })
+    temporaryNumber = id;
+    event.preventDefault();
+}
+
+/*TODO: day du lieu len server tu the form de
+*       thay doi du lieu cua san pham voi id
+*       truyen len*/
+function updatediscount() {
+    let newdiscount = {
+        price: document.getElementById('price-edit').value,
+        color: document.getElementById('color-edit').value,
+        description: document.getElementById('description-edit').value,
+        quantity: document.getElementById('quantity-edit').value,
+        category: {
+            name: document.getElementById('category-edit').value
+        }
+    };
+    $.ajax({
+        headers: {
+            'Accept': 'application/json', 'Content-Type': 'application/json'
+        },
+        type: 'PUT',
+        url: `http://localhost:8080/api/discounts/${temporaryNumber}`,
+        data: JSON.stringify(newdiscount),
+        success: function () {
+            alert('discount updated successfully');
+            displayList();
+        }
+    });
+    event.preventDefault();
+
+}
