d71ff680089d0cbb7d6a0016a4d19508fc7c03a0
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-12 11:36:55 +0700

    fixed add new

--- list/create/index.js
+++ list/create/index.js
@@ -2,12 +2,13 @@ function create() {
     let title = $('#title').val();
     let startDate = $('#startDate').val();
     let endDate = $('#endDate').val();
-    let discountMoney = $('#category').val();
-    let description = $('#description').val();
+    let discountMoney = $('#discountMoney').val();
+    let detail = $('#detail').val();
 
     let newDiscount = {
-        startDate: startDate, description: description, endDate: endDate, discountMoney: discountMoney, title: title
+        startDate: startDate, title: title, endDate: endDate, discountMoney: discountMoney, detail: detail
     }
+    console.log(newDiscount);
     $.ajax({
         headers: {
             'Accept': 'application/json', 'Content-Type': 'application/json'
@@ -18,18 +19,3 @@ function create() {
     event.preventDefault();
 }
 
-function displayCategory(id) {
-    console.log(id);
-    $.ajax({
-        type: 'GET', url: 'http://localhost:8080/api/categories', success: function (data) {
-            let content = `<option selected>The loai</option>
-            `
-            for (let i = 0; i < data.length; i++) {
-                content += `<option value="${data[i].id}">${data[i].name}</option>`;
-            }
-            console.log(content);
-            document.getElementById(`${id}`).innerHTML = content;
-        }
-    });
-    event.preventDefault();
-}
